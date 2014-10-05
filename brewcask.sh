deeb409a3c42602e03e6d3bccd8a95e85df367c3
y@su <toyasyu@gmail.com>
2014-10-05 12:12:45 +0900

    add brewcask.sh

---
+++ brewcask.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# homebrew-cask install
+brew tap caskroom/versions
+brew install caskroom/cask/brew-cask
+
+# dmg install
+# xld書式: %T/%n.%t
+brew cask install alfred bathyscaphe dropbox firefox-ja google-chrome musicbrainz-picard ripit sequel-pro skype sourcetree thunderbird-ja vagrant virtualbox vlc xld xtrafinder sequel-pro sqlite-database-browser
+
+# add Caskroom path
+brew cask alfred link
+
+brew cleanup
+brew cask cleanup
