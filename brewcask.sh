0aa27a39f61c3d9e4e37d4ccde2fa4ad3bdfea21
y@su <toyasyu@gmail.com>
2014-11-29 16:23:47 +0900

    add hipchat, slack

--- brewcask.sh
+++ brewcask.sh
@@ -7,6 +7,7 @@ brew install caskroom/cask/brew-cask
 # dmg install
 # xld書式: %T/%n.%t
 brew cask install alfred bathyscaphe dropbox firefox-ja google-chrome musicbrainz-picard ripit sequel-pro skype sourcetree thunderbird-ja vagrant virtualbox vlc xld xtrafinder sequel-pro sqlite-database-browser keepassx karabiner
+brew cask install hipchat slack
 
 # add Caskroom path
 brew cask alfred link
