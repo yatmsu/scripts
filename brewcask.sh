e7b8338033d332fc7ed4b267fe827532b49a94a4
y@su <toyasyu@gmail.com>
2014-12-01 11:46:45 +0900

    add google-japanese-ime

--- brewcask.sh
+++ brewcask.sh
@@ -8,6 +8,7 @@ brew install caskroom/cask/brew-cask
 # xld書式: %T/%n.%t
 brew cask install alfred bathyscaphe dropbox firefox-ja google-chrome musicbrainz-picard ripit sequel-pro skype sourcetree thunderbird-ja vagrant virtualbox vlc xld xtrafinder sequel-pro sqlite-database-browser keepassx karabiner
 brew cask install hipchat slack
+brew cask install google-japanese-ime
 
 # add Caskroom path
 brew cask alfred link
