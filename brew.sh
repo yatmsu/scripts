7c219f3fdf861bb791248f12f4ca7b9e6c24f7e0
y@su <toyasyu@gmail.com>
2015-01-12 03:14:05 +0900

    add brew.sh

---
+++ brew.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if type brew; then
+else
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+brew install git tig hub
+brew install rbenv ruby-build
+brew install lua
+brew install zsh tmux
+brew install tree curl wget nkf nmap
+brew install phantomjs
+brew install heroku-toolbelt
+brew install sqlite
+brew install mysql
+brew install vim --with-perl --with-lua --override-system-vi
+curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+curl get.pow.cx | sh
