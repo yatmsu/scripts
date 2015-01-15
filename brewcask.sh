47e0304d6b4b9ea09a341c46e0f6d1a75f13ae2d
y@su <toyasyu@gmail.com>
2015-01-15 16:21:56 +0900

    brewcask.sh: update cask installs

--- brewcask.sh
+++ brewcask.sh
@@ -4,16 +4,22 @@
 brew tap caskroom/versions
 brew install caskroom/cask/brew-cask
 
-# dmg install
-# xld書式: %T/%n.%t
-brew cask install bathyscaphe dropbox firefox-ja google-chrome musicbrainz-picard ripit sequel-pro skype sourcetree thunderbird-ja vagrant virtualbox vlc xld xtrafinder sequel-pro sqlite-database-browser keepassx karabiner
+# cask installs
+brew cask install alfred xtrafinder karabiner magican
 brew cask install simple-comic
-brew cask install lastfm
-brew cask install magican
+brew cask install bathyscaphe firefox-ja google-chrome
 brew cask install hipchat slack
 brew cask install google-japanese-ime
-brew cask install vmware-fusion-6
-brew cask install alfred
+brew cask install vagrant vmware-fusion-6 virtualbox
+brew cask install sequel-pro sqlite-database-browser
+brew cask install dropbox
+brew cask install ripit
+brew cask install skype
+brew cask install sourcetree
+brew cask install thunderbird-ja
+brew cask install vlc
+# xld書式: %T/%n.%t
+brew cask install lastfm xld musicbrainz-picard
 
 # start Alfred
 ~/Applications/Alfred\ 2.app/Contents/MacOS/Alfred\ 2
