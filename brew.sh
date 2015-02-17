acfc1aa7a8c0ada2236ea74a4b5d3b076c2f49b8
y@su <toyasyu@gmail.com>
2015-02-17 10:37:23 +0900

    update brew, pow check

--- brew.sh
+++ brew.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if type brew; then
+if type brew
+then
+  echo 'brew already installed'
 else
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
@@ -17,7 +19,9 @@ brew install mysql
 brew install vim --with-perl --with-lua --override-system-vi
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 
-if [ -d ~/.pow ]; then
+if [ -d ~/.pow ]
+then
+  echo 'pow already installed'
 else
   curl get.pow.cx | sh
 fi
