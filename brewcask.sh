00e4518da2b15c6fd3b023c34bb03aebc70d78f8
y@su <toyasyu@gmail.com>
2015-05-17 15:11:49 +0900

    add chefdk

--- brewcask.sh
+++ brewcask.sh
@@ -21,6 +21,7 @@ brew cask install thunderbird-ja
 brew cask install vlc
 brew cask install atom
 brew cask install graphviz
+brew cask install chefdk
 # xld書式: %T/%n.%t
 brew cask install lastfm xld musicbrainz-picard
 
