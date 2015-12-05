d72f9768414e74a1e0ec51b45106ce01237a82c7
y@su <toyasyu@gmail.com>
2015-12-05 19:01:27 +0900

    add xmarks-safari

--- brewcask.sh
+++ brewcask.sh
@@ -22,6 +22,7 @@ brew cask install vlc
 brew cask install atom
 brew cask install graphviz
 brew cask install chefdk
+brew cask install xmarks-safari
 # xld書式: %T/%n.%t
 brew cask install lastfm xld musicbrainz-picard
 
