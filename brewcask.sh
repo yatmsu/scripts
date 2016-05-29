a4433ee1cc18801c8051e9709f919ae89a58902e
y@su <toyasyu@gmail.com>
2016-05-30 00:44:49 +0900

    update homebrew-cask install

--- brewcask.sh
+++ brewcask.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
 # homebrew-cask install
-brew tap caskroom/versions
-brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 
 # cask installs
 brew cask install xtrafinder karabiner magican
 brew cask install simple-comic
 brew cask install bathyscaphe firefox-ja google-chrome
-brew cask install hipchat slack
+brew cask install hipchat slack skype
 brew cask install google-japanese-ime
 brew cask install vagrant vmware-fusion6 virtualbox packer
 brew cask install sequel-pro sqlite-database-browser
 brew cask install dropbox amazon-cloud-drive
 brew cask install ripit
-brew cask install skype
 brew cask install sourcetree
 brew cask install coteditor
 brew cask install thunderbird-ja
