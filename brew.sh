f074eb8dfa43efa38f2896940c0b5f88ab3633b6
y@su <toyasyu@gmail.com>
2017-08-25 23:56:02 +0900

    add ghc cabal-install

--- brew.sh
+++ brew.sh
@@ -13,6 +13,7 @@ brew install ansible
 brew install git git-flow tig hub git-secrets ghq
 brew install rbenv ruby-build rbenv-gemset
 brew install yarn
+brew install ghc cabal-install
 brew install go elixir
 brew install lua
 brew install zsh tmux
