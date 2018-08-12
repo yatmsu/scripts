c4fbfcd7312c3ecdf3a0ca0a2ffbd2994b318e9f
y@su <toyasyu@gmail.com>
2018-08-12 19:30:57 +0900

    add steam

--- brewcask.sh
+++ brewcask.sh
@@ -21,6 +21,7 @@ brew cask install vlc
 brew cask install atom
 brew cask install graphviz
 brew cask install twitch
+brew cask install steam
 # xld書式: %T/%n.%t
 brew cask install lastfm xld musicbrainz-picard
 
