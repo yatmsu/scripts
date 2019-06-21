56058786858043ebef98a7245293595af15c19c0
y@su <toyasyu@gmail.com>
2019-06-21 19:49:22 +0900

    add c-bata/kube-prompt/kube-prompt

--- brew.sh
+++ brew.sh
@@ -11,6 +11,7 @@ brew tap homebrew/versions
 
 brew install ansible
 brew install terraform terraform-docs
+brew install c-bata/kube-prompt/kube-prompt
 brew install kubernetes-helm
 brew install weaveworks/tap/eksctl
 brew install git tig hub git-secrets ghq
